ddc658074607dbe41e89f614ff14c02bcbb3d3ba
nadia <fatmazohra_guidadou@univ-biskra.dz>
2026-04-26 21:37:46 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 session_start();
 
 $host = 'db-mysql-nyc3-10499-do-user-36185384-0.e.db.ondigitalocean.com';
-$dbname = 'medical_practice';
+$dbname = 'defaultdb';
 $username = 'doadmin';
 $password = 'AVNS_bO2G7PtVCtrA6uXCiYp';
 
